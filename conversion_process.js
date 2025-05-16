382f3d16379c111a94417e637b78c87f9bbee14c
hiiragi <123towa.ite@gmail.com>
2025-05-16 19:16:57 +0900

    add kuromoji's converter. in the process of rewriting the script.js.

---
+++ conversion_process.js
@@ -0,0 +1,25 @@
+let tokenizer = null;
+
+// 字句解析(tokenizer)の初期化
+export async function initTokenizer() {
+    if (tokenizer) return;
+
+    tokenizer = await new Promise((resolve, reject) => {
+        kuromoji.builder({ dicPath: './dict/' }).build(function (err, tokenizer) {
+            if (err) reject(err);
+            else resolve(tokenizer);
+        });
+    });
+}
+
+
+
+export function convertToKatakana(text) {
+    if (!tokenizer) {
+        console.error("tokenizerが初期化されていません。");
+        return "";
+    }
+
+    const tokens = tokenizer.tokenize(text);
+    return tokens.map(token => token.reading || token.surface_form);
+}
